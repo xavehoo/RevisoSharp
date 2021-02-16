c481fd66b007c1acc620bafa90b0499e3cfb2a51
Xave <xavehoo@yahoo.it>
2021-02-16 09:34:31 +0100

    Added some classes

---
+++ RevisoSharp/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RevisoSharp
+{
+    class Account
+    {
+        /// <summary>
+        /// The category of the account.
+        /// </summary>
+        public AccountCategory AccountCategory { get; set; }
+
+        /// <summary>
+        /// A link to a list of accounting years for which the account is usable.
+        /// </summary>
+        public AccountingYear AccountingYears { get; set; }
+
+        /// <summary>
+        /// The type of account in the chart of accounts.
+        /// </summary>
+        public EnumAccountType AccountType { get; set; }
+
+        /// <summary>
+        /// Used to bar the account from being used - default is false.
+        /// </summary>
+        public int Balance { get; set; }
+
+        /// <summary>
+        /// Determines if the account can be manually updated with entries.
+        /// </summary>
+        public bool Barred { get; set; }
+
+        public bool BlockDirectEntries { get; set; }
+
+        public Account ContraAccount { get; set; }
+
+        // TODO ---
+        public EnumDebitCredit DebitCredit { get; set; }
+
+        public Department Department { get; set; }
+        // TODO ---
+        public Department DepartmentalDistribution { get; set; }
+
+        public string DisplayNumber { get; set; }
+
+        public Account OpeningAccount { get; set; }
+
+        public bool RequireDepartmentalDistributionOnEntries { get; set; }
+
+        public Account TotalFromAccount { get; set; }
+
+        public Account VatAccount { get; set; }
+
+        public int AccountNumber { get; set; }
+        public string Name { get; set }
+
+    }
+}
---
+++ RevisoSharp/AccountingYear.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace RevisoSharp
+{
+    public class AccountingYear
+    {
+        /// <summary>
+        /// Determines if the accounting year is closed for further transactions.
+        /// </summary>
+        public bool Closed { get; set; }
+
+        /// <summary>
+        /// A link to a collection of all entries booked in the accounting year.
+        /// </summary>
+        public string Entries { get; set; }
+
+        // ! Required
+        // ! Format: full-date (yyyy-MM-dd)
+        /// <summary>
+        /// The first date in the accounting year in the format YYYY-MM-DD. 
+        /// Except for the first accounting year on an agreement, it must be the date immediately following the previous accounting year, and thus must be the first day of a month. 
+        /// The first accounting year on an agreement can begin on any day of the month.
+        /// </summary>
+        public string FromDate { get; set; }
+
+        /// <summary>
+        /// A link to the collection of accounting year periods on an agreement.
+        /// </summary>
+        public string Periods { get; set; }
+
+        // ! Required
+        // ! Format: full-date (yyyy-MM-dd)
+        /// <summary>
+        /// The last date in the accounting year in the format YYYY-MM-DD. 
+        /// It must be the last date in the last month of the accounting year. An accounting year can at most have a duration of 18 months.
+        /// </summary>
+        public string ToDate { get; set; }
+
+        /// <summary>
+        /// A link to the chart of accounts with the years total in base currency.
+        /// </summary>
+        public string Totals { get; set; }
+
+        /// <summary>
+        /// A link to a collection of vouchers created in the accounting year.
+        /// </summary>
+        public string Vouchers { get; set; }
+
+        /// <summary>
+        /// The calendar year or years spanned by the accounting year in the format YYYY or YYYY/YYYY.
+        /// </summary>
+        public string Year { get; set; }
+
+    }
+}
